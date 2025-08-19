c15bd53f51b137f18baad1e232fb133a4cb83bce
Sergey Maltsev <maltsevss84@yandex.ru>
2025-08-19 17:12:22 +0300

    Выполнено задание урока 1

---
+++ script.js
@@ -0,0 +1,14 @@
+// Объявление переменных
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+// Вывод модального окна
+alert("Это модальное окно с текстом из задания!");
+
+// Вывод в консоль
+console.log("Это сообщение выведено в консоль!");
+console.log("Проверка завершена - ошибок нет!");
